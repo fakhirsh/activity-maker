8b138011be577f7d8932a5a6156d68398ca6827b
fakhirsh <fakhir.shaheen@gmail.com>
2024-04-11 23:52:16 +0500

    Code item preview implemented

--- src/components/preview/CodeItem.jsx
+++ src/components/preview/CodeItem.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+// Choose a style or import your own
+import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
-const CodeItem = () => {
+const CodeItem = ({key, content}) => {
   return (
-    <div>CodeItem</div>
+    // <SyntaxHighlighter key={index} language={item.language} style={solarizedlight}>
+    <SyntaxHighlighter key={key} language="python" style={solarizedlight}>
+      {content}
+    </SyntaxHighlighter>
   )
 }
 
